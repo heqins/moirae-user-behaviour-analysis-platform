b2723aab749d706994f3c8f7411c0bf90ed51637
heqin <heqins@hotmail.com>
2023-08-19 19:21:48 +0800

    feat: 更新report-server

---
+++ report-server/src/main/java/com/report/server/ReportMain.java
@@ -0,0 +1,11 @@
+package com.report.server;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportMain {
+
+    public static void main(String[] args) {
+
+    }
+}
